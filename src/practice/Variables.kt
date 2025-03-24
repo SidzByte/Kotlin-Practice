4d74baf81c84b33eb0cd0da369ca2a40a20d6859
SidzByte <sidz007x@gmail.com>
2025-03-25 00:03:34 +0530

    Added Variables.kt code

---
+++ src/practice/Variables.kt
@@ -0,0 +1,11 @@
+package practice
+
+fun main(){
+    val name = "Ishan Dixit"
+    var age = 21
+    println("My friend name is $name & his age is $age")
+
+    age = 60
+    //name = Akash      error due to name changed
+    println("My friend $name is no young he is now aged, as his age is $age")
+}
